189428f6a26306220d48ad5a6e2695a2b4bfeaea
Keenen Cates <kc235@evansville.edu>
2017-09-03 23:05:46 -0500

    Fixed bugs with stack!

--- C/src/stack.c
+++ C/src/stack.c
@@ -11,11 +11,11 @@ void stack_new(stack* stack, int element_size, free_function free_func){
 
 void pop(stack* stack, void* element){
 	assert(list_size(&stack->list) > 0);
-	list_head(&stack->list, &element, TRUE);
+	list_head(&stack->list, element, TRUE);
 }
 
 void push(stack* stack, void* element){
-	list_prepend(&stack->list, &element);
+	list_prepend(&stack->list, element);
 }
 
 void stack_destroy(stack* stack){
--- C/src/stack_test.c
+++ C/src/stack_test.c
@@ -15,28 +15,25 @@ void int_stack(){
 	printf("Creating a stack with the first %d positive ints...\n", nums);
 
 	int i = 1;
-	int elem = 0;
 	stack stack;
 	stack_new(&stack, sizeof(int), NULL);
 	printf("Created the stack!\n\n");
 	for(i = 1; i <= nums; i++){
-		elem = i;
 		printf("Attempting to push onto the stack.\n");
-		push(&stack, &elem);
-		printf("Pushed %d onto the stack.\n\n", elem);
+		push(&stack, &i);
+		printf("Pushed %d onto the stack.\n\n", i);
 	}
-
+	int top = 0;
+	list_head(&stack.list, &top, FALSE);
+	printf("Top of stack is %d\n", top);
 	printf("Pushed %d elements onto the stack.\nNow attempting remove all the elements from the stack..\n\n", nums);
 
-	int* rv = malloc(sizeof(int));
-	*rv = 0; 
+	int rv = 0; 
 	while(nums > 0){
 		printf("Attempting to pop from the stack.\n");
 		pop(&stack, &rv);
-		printf("Got %d from popping the stack.\n\n", *rv);
+		printf("Got %d from popping the stack.\n\n", rv);
 		nums--;
 	}
-	free(rv);
-
 	stack_destroy(&stack);
 }
